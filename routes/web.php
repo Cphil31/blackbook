9659a0aac8f05b8beae5bd132d4c80d129dbdac8
Cphil31 <cecilephilippe31@gmail.com>
2018-05-18 15:08:25 +0200

    creation des routes : email

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('user', 'UserController');
+Route::get('email', 'EmailController@getForm');
+Route::post('email', ['uses' => 'EmailController@postForm', 'as' => 'storeEmail']);
